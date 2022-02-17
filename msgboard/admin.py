6cbe5d6799992820908220c6cdcf98ac9fd46fcd
Shelly Golden <shelly.golden@gmail.com>
2022-02-17 12:05:30 +0200

    Create a messageboard feature

---
+++ msgboard/admin.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# from django.contrib import admin
+=======
+from django.contrib import admin
+>>>>>>> d0a4ba3993c998c2c5287ee1c3d083824d654847
+
+# Register your models here.
---
+++ msgboard/tests.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# from django.test import TestCase
+=======
+from django.test import TestCase
+>>>>>>> d0a4ba3993c998c2c5287ee1c3d083824d654847
+
+# Create your tests here.
